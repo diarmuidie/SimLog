0d5d5007be1c1825435657d9ef4370288958c556
diarmuid <contact@diarmuid.ie>
2013-10-13 10:04:33 +0100

    Typo

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,6 +1,6 @@
     <form class="form-signin" method="post">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="form-control" placeholder="Email address" autofocus name="username">
+        <input type="text" class="form-control" placeholder="Username" autofocus name="username">
         <input type="password" class="form-control" placeholder="Password" name="password" >
         <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Submit">Sign in</button>
     </form>
