72dbfbf46f81c3e12dd6b6cef02f70a99a9723a7
diarmuid <contact@diarmuid.ie>
2013-10-13 10:33:25 +0100

    Fixed Date Formatting

--- application/views/blog/post.php
+++ application/views/blog/post.php
@@ -1,7 +1,7 @@
 <section class="post">
         <article>
                 <h1 class="title"><?php echo $entry['title']; ?></h1>
-                <p class="meta">Posted <?php echo date('NS F Y', strtotime($entry['published']));?></p>
+                <p class="meta">Posted <?php echo date('jS F Y', strtotime($entry['published']));?></p>
                 <?php echo $entry['html']; ?>
         </article>
 
