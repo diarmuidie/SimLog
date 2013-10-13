fbba68062055d5793a84b0f71b8fe0f9b9ef7f4e
diarmuid <contact@diarmuid.ie>
2013-10-13 20:33:26 +0100

    Add 2x retina graphics

--- application/views/template.php
+++ application/views/template.php
@@ -64,12 +64,13 @@
 
 <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.0.3.min.js"><\/script>')</script>-->
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/retina.js"></script>
 
-<script>
+<!--<script>
     var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
         g.src='//www.google-analytics.com/ga.js';
         s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
+</script>-->
 </body>
 </html>
