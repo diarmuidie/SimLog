291239058d04ea1b679b19d32483adcbdb37c828
diarmuid <contact@diarmuid.ie>
2013-10-14 17:31:26 +0100

    Admin template favicon fix
    Fixed the favicon URL on the admin pages. Removed unused author meta tag.

--- application/views/admin/template.php
+++ application/views/admin/template.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
 
     <title><?php echo $title; ?></title>
 
