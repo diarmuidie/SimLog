1bc151984dc9949b56247efd2c656f47680a7761
diarmuid <contact@diarmuid.ie>
2013-10-14 21:35:03 +0100

    Typos

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,1 +1,4 @@
-<p> Hi I'm diarmuid. I work at Intel in Dublin, Ireland as a Manufacturing Engineer. In my free time I build websites and hack together projects with Arduinos.</p>
+<p>
+    Hi I'm Diarmuid. I work at Intel in Dublin, Ireland as a Manufacturing Engineer.
+    In my free time I build websites and hack together projects with Arduinos, bolts and tape.
+</p>
