145a10bce69dfda4a46113e6af97d5fa73ea513d
diarmuid <contact@diarmuid.ie>
2013-10-28 17:20:49 +0000

    Added Google Analytics code

--- application/views/template.php
+++ application/views/template.php
@@ -40,11 +40,16 @@
 <script type="text/javascript" src="<?php echo base_url();?>assets/js/retina.min.js"></script>
 <?php } ?>
 
-<!--<script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_gat._anonymizeIp'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src='//www.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>-->
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-45221135-1', 'diarmuid.ie');
+    ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
