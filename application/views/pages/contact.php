8ed15c422ae6ecbb8bd0dcc35215d7b9a31a2794
diarmuid <contact@diarmuid.ie>
2013-10-29 21:29:17 +0000

    Contact Page style update

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,13 +1,12 @@
 <h1>Find me at:</h1>
 
-<ul>
+<ul id="social">
     <li><a href="http://www.google.com/recaptcha/mailhide/d?k=01-7ulvRuz28c102oT18pnEA==&amp;c=zLUUZt0LG9Bop9A1BUDjHaGOxbVzW3yiaeNvXvgpo9Y=" onclick="window.open('http://www.google.com/recaptcha/mailhide/d?k\07501-7ulvRuz28c102oT18pnEA\75\75\46c\75zLUUZt0LG9Bop9A1BUDjHaGOxbVzW3yiaeNvXvgpo9Y\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;" class="contact">Email</a></li>
     <li><a href="http://ie.linkedin.com/in/diarmuidie" class="linkedin">Linkedin</a></li>
     <li><a href="https://www.facebook.com/diarmuidie" class="facebook">Facebook</a></li>
     <li><a href="https://github.com/diarmuidie" class="github">Github</a></li>
+    <li><a href="https://twitter.com/diarmuidie" class="twitter">Twitter</a></li>
 </ul>
 
-<div id="publickey">
-			<pre><?php echo file_get_contents('8AA7DB0B.asc'); ?></pre>
-    <a href="<?php echo base_url(); ;?>8AA7DB0B.asc">Download Key</a>
-</div>
+
+<a href="<?php echo base_url(); ;?>8AA7DB0B.asc" class="centre">PGP public Key</a> (<a href="http://www.qdog.com/pgp/pgp_faq.html">what is this?</a>)
