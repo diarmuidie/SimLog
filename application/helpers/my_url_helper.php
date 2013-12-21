71cf762342ce9acd3d3494b8ac2ad5e2ad8ce28b
diarmuid <contact@diarmuid.ie>
2013-12-21 21:13:20 +0000

    Overwride for the native url_title function
    Allows accented characters in the URL to be transliterated to their closest ASCII counterpart.

---
+++ application/helpers/my_url_helper.php
@@ -0,0 +1,56 @@
+<?php
+
+if ( ! function_exists('url_title'))
+{
+	/**
+	 * Create URL Title
+	 *
+	 * Takes a "title" string as input and creates a
+	 * human-friendly URL string with a "separator" string
+	 * as the word separator.
+	 *
+	 * @todo	Remove old 'dash' and 'underscore' usage in 3.1+.
+	 * @param	string	$str		Input string
+	 * @param	string	$separator	Word separator
+	 *			(usually '-' or '_')
+	 * @param	bool	$lowercase	Wether to transform the output string to lowercase
+	 * @return	string
+	 */
+	function url_title($str, $separator = '-', $lowercase = FALSE)
+	{
+        $str = iconv('UTF-8', 'ASCII//TRANSLIT', $str); //Transliterate into ASCII
+
+        if ($separator === 'dash')
+		{
+			$separator = '-';
+		}
+		elseif ($separator === 'underscore')
+		{
+			$separator = '_';
+		}
+
+		$q_separator = preg_quote($separator, '#');
+
+		$trans = array(
+				'&.+?;'			=> '',
+				'[^a-z0-9 _-]'		=> '',
+				'\s+'			=> $separator,
+				'('.$q_separator.')+'	=> $separator
+			);
+
+		$str = strip_tags($str);
+		foreach ($trans as $key => $val)
+		{
+			$str = preg_replace('#'.$key.'#i', $val, $str);
+		}
+
+		if ($lowercase === TRUE)
+		{
+			$str = strtolower($str);
+		}
+
+		return trim(trim($str, $separator));
+	}
+}
+
+// ------------------------------------------------------------------------
