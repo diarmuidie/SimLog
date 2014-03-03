7c380f6b924e6cbfe1b43f718b4d8f420ceb50f5
diarmuid <contact@diarmuid.ie>
2014-03-03 18:28:01 +0000

    Add new projects to projects page.

--- application/views/pages/projects.php
+++ application/views/pages/projects.php
@@ -2,4 +2,5 @@
 
 <ul>
     <li><a href="https://github.com/diarmuidie/SimLog">SimLog</a> - The software that powers this site!</li>
+    <li><a href="http://wt.diarmuid.ie/">Warmer Than</a> - A simple weather site that compares the coming weather with yesterdays weather. Alpha site, still being developed.</li>
 </ul>
