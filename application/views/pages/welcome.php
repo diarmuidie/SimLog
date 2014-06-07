7c5e5d3604fdce18d46b9b9401297f0a753b7e8d
Diarmuid <contact@diarmuid.ie>
2014-06-07 16:39:13 +0100

    Update welcome.php
    Typo

--- application/views/pages/welcome.php
+++ application/views/pages/welcome.php
@@ -1,4 +1,4 @@
 <p>
-    Hi I'm Diarmuid. I work at <A href="http://daft.ie/about">Daft.ie</a> in Dublin, Ireland as a Web Developer.
+    Hi I'm Diarmuid. I work at <a href="http://daft.ie/about">Daft.ie</a> in Dublin, Ireland as a Web Developer.
     In my free time I build websites and hack together projects with Arduinos, bolts and tape.
 </p>
