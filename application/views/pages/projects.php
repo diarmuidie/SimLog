a3e016753eecad1ca460bf31ab8f89816334837b
Diarmuid <contact@diarmuid.ie>
2014-06-07 16:46:11 +0100

    Update projects.php
    Commit live changes back into git

--- application/views/pages/projects.php
+++ application/views/pages/projects.php
@@ -1,6 +1,7 @@
 <h1>Some of My Projects:</h1>
 
 <ul>
+    <li><a href="http://archive.diarmuid.ie">Diarmuid.ie Version 1.0</a> - For posterity I've moved my old site and blog to a subdomain. Built on wordpress with blood sweat and tears!</li>
     <li><a href="https://github.com/diarmuidie/SimLog">SimLog</a> - The software that powers this site!</li>
     <li><a href="http://wt.diarmuid.ie/">Warmer Than</a> - A simple weather site that compares the coming weather with yesterdays weather. Alpha site, still being developed.</li>
 </ul>
